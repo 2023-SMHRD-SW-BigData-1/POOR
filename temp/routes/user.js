a4f06c379a804b0911e138fd94ca829430178b73
geonil <wjd6611@gmail.com>
2023-07-07 11:19:16 +0900

    push

--- temp/routes/user.js
+++ temp/routes/user.js
@@ -7,37 +7,52 @@ const router = express.Router()
 router.use(express.json())
 router.use(express.urlencoded({extended:false}));
 const app = express()
-oracledb.initOracleClient({libDir: 'C:/Users/smhrd/Desktop/temp/oracle_client'})
+// oracledb.initOracleClient({libDir: 'C:/Users/smhrd/Desktop/temp/oracle_client'})
 
-
-
-
-router.get('/home', async (req, res)=>{
-    let connection
-    let result
-    try{
-        connection =await oracledb.getConnection({
-            user          : "campus_h_230627_1",
-            password      : "smhrd1",
-            connectString : "project-db-stu2.smhrd.com:1524/"
-        })
-
-       result = await connection.execute('SELECT * FROM T_USER');
-
-    console.log(result.rows);
-} catch (error) {
-    console.log(error)
-} finally {
-    if (connection) {
-        try {
-            await connection.close()
-        } catch (error) {
-            console.log(error)
-        }
+oracledb.initOracleClient({libDir: 'oracle_client'})
+let conn;
+oracledb.getConnection({
+    user : "campus_h_230627_1",
+    password : "smhrd1",
+    connectString : "project-db-stu2.smhrd.com:1524/"
+},(err,con)=>{
+    if(err){
+        console.log('접속실패',err);
     }
-}   
-console.log(result.rows);
+    conn = con;
+});
+
+const handleLogin = async(id,pw)=>{
+    try{
+        const sql = "SELECT * FROM T_USER WHERE USER_ID = '"+id+"' AND USER_PW = '"+pw+"'";
     
+
+        let result;
+        result = await conn.execute(sql)
+
+            if (result.rows.USER_ID !== '') {
+              // 로그인 성공
+              console.log('로그인 성공');
+            } else {
+              // 로그인 실패
+              console.log('로그인 실패');
+            }
+            count=0
+        
+            await conn.close()
+    }catch(err){
+        console.log(err);
+    }
+
+}
+
+app.use(handleLogin)
+
+
+router.post('/', (req, res)=>{
+    handleLogin(req.body.id,req.body.pw)
+    res.sendFile(path.join(__dirname,'/react-project/build/index.html'))
+
    
 })
 
